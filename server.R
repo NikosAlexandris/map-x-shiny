860c8db7805abfa356ea01526524d08b1e995392
fxi <f@fxi.io>
2015-11-24 16:53:19 +0100

    cleaned settings/config list.

--- server.R
+++ server.R
@@ -7,6 +7,17 @@
 #                 |_|           
 # main server function
 
+# R libraries (managed using packrat !)
+source("loadlib.R")
+# map-x functions (this will be transfered to a R package)
+source("helper/R/mapx.R")
+# handson table functions
+source('helper/R/handson.R')
+# general configuration, site independant.
+source("settings/config-global.R")
+# configuration specific to the host on wich map-x is launched
+source("settings/config-local.R")
+
 #
 # Define main user interface
 #
@@ -64,23 +75,17 @@ shinyServer(function(input, output, session) {
     #
     # Load server parts
     #
-    # 
     # Navigation and login
-    #
     source("parts/server/login.R",local=TRUE)
     source("parts/server/nav.R",local=TRUE)
     source("parts/server/urlParsing.R",local=TRUE)
-    #
     # Country panel
-    #
     observe({
       if(mxReact$allowCountry){
         source("parts/server/country.R",local=TRUE)
       }
     })
-    #
     # Map panel
-    #
     observe({
       if(mxReact$allowMap){
         source("parts/server/map.R",local=TRUE)
@@ -88,9 +93,7 @@ shinyServer(function(input, output, session) {
         source("parts/server/tenke.R",local=TRUE)
       }
     })
-    #
     # Administration panel
-    #
     observe({
       if(mxReact$allowAdmin){
         source("parts/server/admin.R",local=TRUE)
--- settings/settings-base.R
+++
@@ -1,4 +0,0 @@
-
-
-dbInfo=list(host='127.0.0.1',dbname='',port='',user='',password='')
-remoteInfo = list(host="127.0.0.1",user="vagrant",port=2222)
