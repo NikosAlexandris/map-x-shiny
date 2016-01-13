5d7b45405b964ad525efd0eadfba999a2a0b48f2
fxi <f@fxi.io>
2016-01-13 18:21:36 +0100

    tour with bootstrapTour : only the visible part

--- www/language/tours.js
+++ www/language/tours.js
@@ -9,35 +9,49 @@
  http://bootstraptour.com/api/
 
 */
-
-
-
-var tours = {};
 var txt = {};
-
-
-
 
 /* tour text by language */
 
 
-txt = {
-  "#btnLogin":{
-    en:{
+mxTour = {
+
+  "btnNavHome":{
+    eng:{
+      title:"Home",
+      content:"Button leading to the top section"
+    },
+    fre:{
+      title:"Accueil",
+      content:"Lien vers la page d'accueil"
+    }
+  },
+  "btnNavMap":{
+    eng:{
+      title:"Map",
+      content:"Button leading to the map section"
+    },
+    fre:{
+      title:"Carte",
+      content:"Lien vers la carte interactive"
+    }
+  },
+  "btnLogin":{
+    eng:{
       title:"Authentication",
       content:"Button to confirm credential and launch map-x"
     },
-    fr:{
+    fre:{
       title:"Authentification",
       content:"Bouton pour s'authentifier et accéder à l'application"
     }
   },
-  "#btnLogout":{
-    en:{
+  "btnLogout":{
+    eng:{
       title:"Sign out",
       content:"Button to sign out"
     },
-    fr:{
+    fre:{
       title:"Quitter",
       content:"Bouton pour quitter l'application"
     }
@@ -46,29 +60,63 @@ txt = {
 
 
 
+/* function to check if elemnt is in view port */
+/* http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling */
+function isVisible(e) {
+    var el = document.getElementById(e);
+    var elemTop = el.getBoundingClientRect().top;
+    var elemBottom = el.getBoundingClientRect().bottom;
+    var inViewPort = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return inViewPort;
+}
+
+
 /*  Populate steps */
 
 
-stepsIntro = [];
-res = {};
+
 lang = "en";
-for(var el in txt){
- res = { 
-   element:el,
-   title:txt[el][lang].title,
-   content:txt[el][lang].content
- };
- stepsIntro.push(res);
+
+
+function mxStartTour(mxTour,language){
+  
+  var steps= [];
+  var res = {};
+  var lang ;
+
+  if(typeof language == "undefined" ){
+    lang = $("#selectLanguage").val();
+    if(typeof lang == "undefined" ){
+      lang = "eng" ;
+    }
+  }else{
+    lang = language;
+  }
+
+  for(var el in mxTour){
+    if(isVisible(el)){
+      res = { 
+        element:"#"+el,
+        title:mxTour[el][lang].title,
+        content:mxTour[el][lang].content
+      };
+      steps.push(res);
+    }
+  }
+  /* Tour creation */
+
+  tour =  new Tour({
+    backdrop: false,
+    steps : steps,
+    storage : false,
+    autoscroll: false
+  });
+
+  tour.init();
+  tour.start(true);
+
 }
 
-/* Tour creation */
 
-tours.intro =  new Tour({
-  steps : stepsIntro
-});
-
-/* Initiat tour*/
-
-tours.intro.init();
 
 
