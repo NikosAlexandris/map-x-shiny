b7e0c6eb5e12a75ffb1356af5bdd7624ecb87a46
fxi <f@fxi.io>
2016-02-18 15:38:52 +0100

    default title = no data

--- parts/ui/map.R
+++ parts/ui/map.R
@@ -43,7 +43,7 @@ uiMapCreator <- tagList(
             )
           ),
         "style"=list("title"="Create a view",content=tagList(
-            textInput("mapViewTitle","Title","Default"),
+            textInput("mapViewTitle","Title",mxConfig$noData),
             selectInput("selLayer","Layer",choices=""),
             selectInput("selColumnVar","Variable to display",choices=""),
             textInput("txtVarUnit","Suffix for labels"),
